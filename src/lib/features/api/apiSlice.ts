02729cd65f040b5b01eee7d8c023bc3863a66e68
Versus Development <bockster6669@gmail.com>
2024-10-22 19:18:39 +0300

    update

--- src/lib/features/api/apiSlice.ts
+++ src/lib/features/api/apiSlice.ts
@@ -1,9 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'https://blog-app-bay-one.vercel.app/api'
-    : 'http://localhost:3000/api';
+const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
