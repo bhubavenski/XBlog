3339810511c6bb852d01cb8b0ec0445b4ae2d75d
Versus Development <bockster6669@gmail.com>
2024-11-02 19:28:34 +0300

    update

--- src/lib/features/api/apiSlice.ts
+++ src/lib/features/api/apiSlice.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}/api`
+const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
   : 'http://localhost:3000/api';
 
 export const apiSlice = createApi({
